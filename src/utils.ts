b6d9cff5720950eb7ae71545ca8bf2eacab4e70e
WanderVafla <procenkovladik1@gmail.com>
2026-05-06 08:21:23 +0200

    refactor: rename checkColorDark to isColorLight and update logic for clarity

--- src/utils.ts
+++ src/utils.ts
@@ -17,10 +17,10 @@ export const getDaysDueDiff = (due: string): number | null => {
   return null
 }
 
-export const checkColorDark = (rgb: string): boolean => {
-  const regex = /\d{1,3}.\s\d{1,3}.\s\d{1,3}/g
-  const parsetColorRgb = String(regex.exec(rgb)).split(', ')
-  const [r, g, b] = parsetColorRgb.map(Number)
+export const isColorLight = (rgb: string): boolean => {
+  const match = rgb.match(/\d+/g)
+  if (!match || match.length < 3) return false
+  const [r, g, b] = match.map(Number)
   const luminance = (0.299 * r + 0.587 * g + 0.144 * b) / 255
   return luminance > 0.5
 }
